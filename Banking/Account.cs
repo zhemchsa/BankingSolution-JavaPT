5e7d4f31e41c57ffe63db8d94f573c9372c9d8b3
Sergey Zhemchuzhny <sergey.zhemchuzhny@gmail.com>
2022-01-19 21:36:58 -0500

    Add project files.

---
+++ Banking/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking {
+    public class Account {
+
+        public int Id { get; private set; }
+        public decimal Balance { get; private set; } = 0;
+        public string Description { get; set; }
+
+
+        public void Deposit(decimal Amount) {
+            Balance += Amount;
+        }
+        public void Withdraw(decimal Amount) {
+            if (Amount > Balance) {
+                throw new Exception("Insufficient funds");
+                }
+            Balance -= Amount;
+        }
+
+        public Account(int Id) {
+            this.Id = Id;             
+        }
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct1 = new Account(1);
+            acct1.Deposit(100);
+        }
+    }
+}
